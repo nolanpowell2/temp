8f367de116a209a5b6b28c70b0401edcb29a7d9c
nolanpowell2 <nolanhpowell.2@gmail.com>
2026-07-01 13:53:15 -0500

    Add documentation

--- GolfAim.java
+++ GolfAim.java
@@ -7,36 +7,46 @@ import java.awt.event.InputEvent;
 import java.awt.AWTException;
 import java.awt.MouseInfo;
 
+/**
+ * GolfAim listens for global hotkeys (Alt + specific keys) to allow
+ * pixel-perfect mouse adjustments and clicking for the SkyTrac simulator.
+ */
 public class GolfAim implements NativeKeyListener
 {
-
+    // Robot instance handles the mouse movememet and left-clicking
     private Robot r;
 
+    // Handles global key presses across the OS
     public void nativeKeyPressed(NativeKeyEvent e)
     {
-
+        // checks wether alt is currently being pressed 
         boolean isAlt = (e.getModifiers() & NativeKeyEvent.ALT_MASK) != 0;
 
+        // if alt and ' keys are being pressed, move the mouse right using the class's robot object
         if (isAlt && e.getKeyCode() == NativeKeyEvent.VC_QUOTE)
         {
             r.mouseMove(MouseInfo.getPointerInfo().getLocation().x + 1, MouseInfo.getPointerInfo().getLocation().y);
         }
 
+        // if alt and ; keys are being pressed, move the mouse left using the class's robot object
         else if (isAlt && e.getKeyCode() == NativeKeyEvent.VC_SEMICOLON)
         {
             r.mouseMove(MouseInfo.getPointerInfo().getLocation().x - 1, MouseInfo.getPointerInfo().getLocation().y);
         }
 
+        // if alt and [ keys are being pressed, move the mouse up using the class's robot object
         else if (isAlt && e.getKeyCode() == NativeKeyEvent.VC_OPEN_BRACKET)
         {
             r.mouseMove(MouseInfo.getPointerInfo().getLocation().x, MouseInfo.getPointerInfo().getLocation().y - 1);
         }
 
+        // if alt and / keys are being pressed, move the mouse down using the class's robot object
         else if (isAlt && e.getKeyCode() == NativeKeyEvent.VC_SLASH)
         {
             r.mouseMove(MouseInfo.getPointerInfo().getLocation().x, MouseInfo.getPointerInfo().getLocation().y + 1);
         }
 
+        // if alt and a keys are being pressed, left click using the class's robot object to lock in shot
         else if (isAlt && e.getKeyCode() == NativeKeyEvent.VC_A)
         {
             r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
@@ -44,6 +54,7 @@ public class GolfAim implements NativeKeyListener
         }
     }
 
+    // setter for the Robot instance variable
     public void setRobot(Robot r)
     {
         this.r = r;
@@ -51,9 +62,10 @@ public class GolfAim implements NativeKeyListener
 
     public static void main(String[] args)
     {
-
+        // create a new GolfAim object
         GolfAim aimer = new GolfAim();
 
+        // Initialize the Robot for mouse control
         try 
         {
             aimer.setRobot(new Robot());
@@ -72,6 +84,7 @@ public class GolfAim implements NativeKeyListener
 
         }
         
+        // add the GolfAim object to the GlobalScreen
         GlobalScreen.addNativeKeyListener(aimer);
 
     }
